9d16cbe9cfe4cd5b7cfc80965a7934e3381ab200
Emily Xavier <m.risse.dev@gmail.com>
2022-05-18 09:27:00 -0300

    Introdução a métodos :rocket:

--- src/academy/devdojo/maratonajava/javacore/introducaometodos/dominio/Funcionario.java
+++ src/academy/devdojo/maratonajava/javacore/introducaometodos/dominio/Funcionario.java
@@ -1,9 +1,10 @@
 package academy.devdojo.maratonajava.javacore.introducaometodos.dominio;
 
 public class Funcionario {
-    public String nome;
-    public int idade;
-    public double[] salarios;
+    private String nome;
+    private int idade;
+    private double media = 0;
+    private double[] salarios;
 
     public void imprime(){
         System.out.println("Funcionário: ");
@@ -16,12 +17,44 @@ public class Funcionario {
         imprimeMediaSalarial();
     }
     public void imprimeMediaSalarial(){
-        double media = 0;
+        if (salarios == null){
+            return;
+        }
+
         for (double salario: salarios){
             media += salario;
         }
         media /= salarios.length;
+
         System.out.println("\nMédia Salarial: "+media);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double[] getSalarios() {
+        return salarios;
+    }
+
+    public void setSalarios(double[] salarios) {
+        this.salarios = salarios;
+    }
+
+    public double getMedia() {
+        return media;
     }
 
 }
--- src/academy/devdojo/maratonajava/javacore/introducaometodos/test/FuncionarioTest01.java
+++ src/academy/devdojo/maratonajava/javacore/introducaometodos/test/FuncionarioTest01.java
@@ -6,9 +6,9 @@ public class FuncionarioTest01 {
     public static void main(String[] args) {
         Funcionario funcionario = new Funcionario();
 
-        funcionario.nome = "Sanji";
-        funcionario.idade = 20;
-        funcionario.salarios = new double[] {1200, 987.32, 2000};
+        funcionario.setNome("Sanji");
+        funcionario.setIdade(23);
+        funcionario.setSalarios(new double[] {1200, 987.32, 2000});
 
         funcionario.imprime();
     }
