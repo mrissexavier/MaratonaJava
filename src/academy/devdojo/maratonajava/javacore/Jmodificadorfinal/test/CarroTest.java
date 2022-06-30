a73b214189244d8d4929c356f6680fa7cc4971ec
mrissexavier <m.risse.dev@gmail.com>
2022-06-30 10:51:18 -0300

    Modificador final

---
+++ src/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/test/CarroTest.java
@@ -0,0 +1,24 @@
+package academy.devdojo.maratonajava.javacore.Jmodificadorfinal.test;
+
+import academy.devdojo.maratonajava.javacore.Jmodificadorfinal.dominio.Carro;
+import academy.devdojo.maratonajava.javacore.Jmodificadorfinal.dominio.Comprador;
+import academy.devdojo.maratonajava.javacore.Jmodificadorfinal.dominio.Ferrari;
+
+import java.util.Comparator;
+
+public class CarroTest {
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+
+        System.out.println(Carro.VELOCIDADE_LIMITE);
+        System.out.println(carro.COMPRADOR);
+        carro.COMPRADOR.setNome("Emily");
+        System.out.println(carro.COMPRADOR);
+
+        System.out.println("===========================");
+
+        Ferrari ferrari = new Ferrari();
+        ferrari.setNome("Emily");
+        ferrari.imprime();
+    }
+}
