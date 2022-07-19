57f3d834fbc2b9a8a766d4189ffc86c7b46942f5
mrissexavier <m.risse.dev@gmail.com>
2022-07-19 08:41:55 -0300

    Exceção

---
+++ src/academy/devdojo/maratonajava/javacore/Oexception/error/test/StackOverflowTest.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratonajava.javacore.Oexception.error.test;
+
+public class StackOverflowTest {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Oexception/runtime/test/RuntimeExceptionTest.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.javacore.Oexception.runtime.test;
+
+public class RuntimeExceptionTest {
+    //Checked e Unchecked
+
+    /*Exceções "Checked" são exceções que ja foram "checadas", filhas diretas da classe Exception
+    * E se não tratadas elas irão lançar um erro em tempo de compilação (ou seja, o código não irá compilar)
+    */
+
+    /*Exceções "Unchecked" são da classe RuntimeException ou filas da mesma, quando lançasdas pelo programa, quase 100%
+    * das vezes o problema está no seu código
+     */
+
+    /*
+    * Excações unchecked não são obrigadas a serem tratadas, contrário das exceções checked que obrigatóriamente precisam
+    * ser tratadas
+    */
+
+
+}
